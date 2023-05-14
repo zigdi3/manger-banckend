99cd39dcd32fadd8480a48363acf65a1ecd74bc3
Diego Araujo <diegozigoto@gmail.com>
2023-05-14 03:08:35 -0300

    Build initial app

---
+++ src/app.module.ts
@@ -0,0 +1,12 @@
+import { ProductModule } from './modules/products/product.module';
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+@Module({
+  imports: [
+    ProductModule,],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule { }
